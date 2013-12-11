92da1874f4e3da2e4eb6cdca7fe33aba84e997a7
zz chen <grantchen88@gmail.com>
2013-12-11 20:25:34 +0800

    1st commit

---
+++ 1.scm
@@ -0,0 +1,24 @@
+#! /usr/bin/guile -s 
+!#
+
+(define (square x) (* x x))
+
+(define (averageof x y) (/ (+ x y) 2.0))
+
+(define (zabs x)
+ (if (< x 0) (- x) x))
+
+(define (good_enough guess x)
+ (< (zabs (- (square guess) x)) 0.001))
+
+(define (improve guess x) (averageof guess (/ x guess)))
+
+(define (sqrt_iter guess x)
+ (if (good_enough guess x) guess 
+  (sqrt_iter (improve guess x) x)))
+
+(define (zsqrt x) 
+ (if (< x 0) (- 1) (sqrt_iter 1.0 x)))
+
+(display (zsqrt 8.9))
+(display "\n")
